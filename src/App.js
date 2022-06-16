2b2cd39e5bc022e45d20fe673811be426abde2d1
unganemo <76483254+unganemo@users.noreply.github.com>
2022-06-16 23:04:06 +0200

    Initial Commit
    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const [alph, setAlph] = useState(alphabet.split(''))
+  const [word, setWord] = useState([])
+  const [yellow, setYellow] = useState([])
+  const [grey, setGrey] = useState([])
+  const length = 5
+
+  const setInitWord = () => {
+    for(let i = 0; i < length; i++) {
+      word.push({
+        letter: alph[Math.floor(Math.random() * alph.length)],
+        green: false,
+        position: i
+      }) 
+      console.log("hej " + word[i].letter + word[i].green)
+    }
+  }
+
+  const updateWord = (index, letter) => {
+
+  }
+
+  useEffect(() => {
+    setInitWord()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder='Yellow letters' onChange={(event) => setYellow(event.target.value)}></input>
+      <input placeholder='Grey letters' onChange={(event) => setGrey(event.target.value)}></input>
+      <p>{alph}</p>
+      <p>{grey}</p>
+      <div>
+        {word.map(obj => {
+          return(
+            <div>
+              <p>Position {obj.position}</p>
+              <input value={obj.letter} onChange={(event) => updateWord(obj.position, event.target.value)}></input>
+            </div>
+          )
+          
+        })}
+      </div>
     </div>
   );
 }
